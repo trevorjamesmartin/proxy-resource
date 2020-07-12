0628ca7f8699f8f9d1495c19fb35cf81adb76528
Trevor Martin <trevor.j.martin@gmail.com>
2020-07-11 20:00:14 -0400

    test

--- index.js
+++ index.js
@@ -25,41 +25,47 @@ app.get("/palettes/ch/:number", (req, res) => {
     }
   } catch {
     console.log("error");
+    res.status(404).json([]);
   }
-  request(result, (err, response, body) => {
-    if (err || response.statusCode !== 200) {
-      return res.status(500).json({ type: "error", message: err.message });
+  palette.find({ id: n }).then((existing) => {
+    // try to find in db
+    if (existing && existing.id == n) {
+      res.status(200).json(existing); // return existing record
     }
-    const txt = body
-      .split("\n")
-      .filter((line) => line.match(/itemer/))
-      .map((i) => i.split(";")[0])[0]
-      .split(",")[2]
-      .replace(" ", "");
-    const colorString = txt.replace(/[^a-z0-9]/gi, "");
-    const colorOne = `#${colorString.substring(0, 6)}`;
-    const colorTwo = `#${colorString.substring(6, 12)}`;
-    const colorThree = `#${colorString.substring(12, 18)}`;
-    const colorFour = `#${colorString.substring(18)}`;
-    // new record?
-    const origin = result.url;
-    const colors = JSON.stringify([colorOne, colorTwo, colorThree, colorFour]);
-    const id = req.params.number;
-    const record = { id, colors, origin };
-    // const table = "palette";
-    palette.find({ id }).then((existing) => {
-      if (!existing || (existing && !existing.id)) {
-        palette.add(record).then((result) => {
-          console.log(result);
-          res
-            .status(200)
-            .json({ origin, colorOne, colorTwo, colorThree, colorFour });
-        });
-      } else {
-        res
-          .status(200)
-          .json({ origin, colorOne, colorTwo, colorThree, colorFour });
+    // fetch from colorHunt
+    request(result, (err, response, body) => {
+      if (err || response.statusCode !== 200) {
+        return res.status(500).json({ type: "error", message: err.message });
       }
+      const txt = body
+        .split("\n")
+        .filter((line) => line.match(/itemer/))
+        .map((i) => i.split(";")[0])[0]
+        .split(",")[2]
+        .replace(" ", "");
+      const colorString = txt.replace(/[^a-z0-9]/gi, "");
+      const colorOne = `#${colorString.substring(0, 6)}`;
+      const colorTwo = `#${colorString.substring(6, 12)}`;
+      const colorThree = `#${colorString.substring(12, 18)}`;
+      const colorFour = `#${colorString.substring(18)}`;
+      // new record?
+      const origin = result.url;
+      const colors = JSON.stringify([
+        colorOne,
+        colorTwo,
+        colorThree,
+        colorFour,
+      ]);
+      const id = req.params.number;
+      const record = { id, colors, origin };
+      // const table = "palette";
+    });
+
+    palette.add(record).then((result) => {
+      console.log(`added palette ${result}`);
+      res
+        .status(200)
+        .json({ origin, colorOne, colorTwo, colorThree, colorFour });
     });
   });
 });
