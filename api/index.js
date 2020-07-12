63576d43b7ad3e5c326dbdec1f26ac6140ae67ea
Trevor Martin <trevor.j.martin@gmail.com>
2020-07-11 20:51:59 -0400

    router

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const palette = require("./palette");
+const GetPalette = require("./colorhunt");
+router.get("/ch/:number", (req, res) => {
+  // return HTML color codes
+  const ch = { url: "error" };
+  const n = Number(req.params.number);
+  if (n > 0) {
+    ch.url = `${process.env.CHP_URL}${n}`;
+  }
+  // result contains possibly valid url. check if db record exists
+  palette
+    .find({ id: n })
+    .then((existing) => {
+      // try to find in db
+      existing && existing.id === n
+        ? res.status(200).json(existing) // return existing record
+        : GetPalette({ id: n, result: ch, palette }); // fetch from url
+    })
+    .catch((err) => {
+      // console.log(err);
+      console.log("catch");
+      GetPalette({ id: n, result: ch, palette, res }); // fetch from url
+    }); // end pallete.find
+});
+module.exports = router;
--- index.js
+++ index.js
@@ -1,41 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const router = require("./api");
+const { Server } = require("http");
 const app = express();
-const palette = require("./api/palette");
-const GetPalette = require("./api/colorhunt");
+
 try {
   require("dotenv").config();
 } catch {
   console.log("not loading dotenv");
 }
 
-app.use(cors());
 app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use("/palettes", router);
 app.get("/", (req, res) => {
   res.status(200).send("OK");
-});
-app.get("/palettes/ch/:number", (req, res) => {
-  // return HTML color codes
-  const ch = { url: "error" };
-  const n = Number(req.params.number);
-  if (n > 0) {
-    ch.url = `${process.env.CHP_URL}${n}`;
-  }
-  // result contains possibly valid url. check if db record exists
-  palette
-    .find({ id: n })
-    .then((existing) => {
-      // try to find in db
-      existing && existing.id === n
-        ? res.status(200).json(existing) // return existing record
-        : GetPalette({ id: n, result: ch, palette }); // fetch from url
-    })
-    .catch((err) => {
-      // console.log(err);
-      console.log("catch");
-      GetPalette({ id: n, result: ch, palette, res }); // fetch from url
-    }); // end pallete.find
 });
 
 const PORT = process.env.PORT || 4000;
